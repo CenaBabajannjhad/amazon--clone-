f541ee5c68059ffaf7319ddfc68ec99c69c75f2f
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-08-03 15:27:40 +0330

    add infinite scroll data

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,251 @@
 "use strict"
 
+// **boxes data**
+const Data = [
+  {
+    title: "product-1",
+    imageSrc: "./assets/image/boxes-img/1.jpg",
+  },
+  {
+    title: "product-2",
+    imageSrc: "./assets/image/boxes-img/2.jpg",
+  },
+  {
+    title: "product-3",
+    imageSrc: "./assets/image/boxes-img/3.jpg",
+  },
+  {
+    title: "product-4",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-5",
+    imageSrc: "./assets/image/boxes-img/5.jpg",
+  },
+  {
+    title: "product-6",
+    imageSrc: "./assets/image/boxes-img/6.jpg",
+  },
+  {
+    title: "product-7",
+    imageSrc: "./assets/image/boxes-img/7.jpg",
+  },
+  {
+    title: "product-8",
+    imageSrc: "./assets/image/boxes-img/8.jpg",
+  },
+  {
+    title: "product-9",
+    imageSrc: "./assets/image/boxes-img/9.jpg",
+  },
+  {
+    title: "product-10",
+    imageSrc: "./assets/image/boxes-img/10.jpg",
+  },
+  {
+    title: "product-11",
+    imageSrc: "./assets/image/boxes-img/11.jpg",
+  },
+  {
+    title: "product-12",
+    imageSrc: "./assets/image/boxes-img/12.jpg",
+  },
+  {
+    title: "product-13",
+    imageSrc: "./assets/image/boxes-img/13.jpg",
+  },
+  {
+    title: "product-14",
+    imageSrc: "./assets/image/boxes-img/14.jpg",
+  },
+  {
+    title: "product-15",
+    imageSrc: "./assets/image/boxes-img/15.jpg",
+  },
+  {
+    title: "product-16",
+    imageSrc: "./assets/image/boxes-img/16.jpg",
+  },
+  {
+    title: "product-17",
+    imageSrc: "./assets/image/boxes-img/17.jpg",
+  },
+  {
+    title: "product-18",
+    imageSrc: "./assets/image/boxes-img/18.jpg",
+  },
+  {
+    title: "product-19",
+    imageSrc: "./assets/image/boxes-img/19.jpg",
+  },
+  {
+    title: "product-20",
+    imageSrc: "./assets/image/boxes-img/20.jpg",
+  },
+  {
+    title: "product-21",
+    imageSrc: "./assets/image/boxes-img/21.jpg",
+  },
+  {
+    title: "product-22",
+    imageSrc: "./assets/image/boxes-img/22.jpg",
+  },
+  {
+    title: "product-23",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-24",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-25",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-26",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-27",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-28",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-29",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-30",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-31",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-32",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-33",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-34",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-35",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-36",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-37",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-38",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-39",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-40",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-41",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-42",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-43",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-44",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-45",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-46",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-47",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-48",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-49",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-50",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-51",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-52",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-53",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-54",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-55",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-56",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-57",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-58",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-59",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+  {
+    title: "product-60",
+    imageSrc: "./assets/image/boxes-img/4.jpg",
+  },
+];
+const boxesRoot = document.querySelector("#box-container");
+let scrollBoxesCount = 8;
+let scrollStep = 0;
 // **carousel area**
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
